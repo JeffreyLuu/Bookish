23abda533fef8fc2203492cce113fda35f9750d8
root <root@intcargil.zoo.lan>
2019-07-12 11:23:30 +0100

    added orm

--- src/libraryParser.ts
+++ src/libraryParser.ts
@@ -5,15 +5,72 @@ import User from "./model_user"
 import { resolve } from "url";
 import path from 'path';
 import { copyFile } from "fs";
+import Sequelize from 'sequelize'
+const sequelize = new Sequelize('postgres://bookish:SoftwireBookish@[::1]:5432/bookish')
 const app = express();
 const port =  3000;
 var pgp = require('pg-promise')();
 var db = pgp('postgres://bookish:SoftwireBookish@[::1]:5432/bookish')
 
+const Book = sequelize.define('book', {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    author: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    isbn: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      }
+  }, {
+    freezeTableName: true
+});
+
+const User = sequelize.define('user', {
+    username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+});
+
+const Copy = sequelize.define('copies', {
+  }, {
+    freezeTableName: true
+});
+
+Copy.belongsTo(Book, {foreignKey: 'book_id', allowNull: false});
+Copy.belongsTo(User, {foreignKey: 'user_id'});
+
+const Transaction = sequelize.define('transactions', {
+    date_borrowed: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+      },
+    date_returned: {
+        type: Sequelize.DATEONLY
+      },
+    due_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+      },
+}, {
+  freezeTableName: true
+});
+
+Transaction.belongsTo(Copy, {foreignKey: 'copy_id', allowNull: false});
+Transaction.belongsTo(User, {foreignKey: 'user_id', allowNull: false});
 
 export async function getAllBooks () {
     
-    let allBooks = await db.any('SELECT * FROM public.book');
+    let allBooks = await Book.findAll();
     try{
         let listOfBooks = [];
         for (let rawBook of allBooks){
@@ -28,7 +85,11 @@ export async function getAllBooks () {
 }
 
 export async function getUsers(username, password){
-    let user = await db.one(`SELECT * FROM public.users WHERE username = '${username}';`);
+    let user = await User.findAll({
+        where: {
+          username: `${username}`
+        }
+      });
     
     try{
         return (user['password'] === password);
@@ -39,7 +100,11 @@ export async function getUsers(username, password){
 
 async function getUserTransaction (username) {
     
-    let user = await db.one(`SELECT * FROM public.users WHERE username = '${username}'`);
+    let user = await User.findAll({
+        where: {
+          username: `${username}`
+        }
+    });
     let user_id = user['id'];
     let userTransactions = await db.any(`SELECT * FROM public.transactions WHERE user_id = ${user_id}`);
     return userTransactions;
@@ -47,9 +112,17 @@ async function getUserTransaction (username) {
 
 async function getBookName (copy_id) {
     
-    let copy = await db.one(`SELECT * FROM public.copies WHERE id = '${copy_id}'`);
+    let copy = await Copy.findAll({
+        where: {
+          copy_id: `${copy_id}`
+        }
+    });
     let book_id = copy.book_id
-    let book = await db.one(`SELECT * FROM public.book WHERE id = '${book_id}'`);
+    let book = await Book.findAll({
+        where: {
+          book_id: `${book_id}`
+        }
+    });
     let book_name = book.name
     return book_name
 }
